270c49a67a5c2fa619d58d573921faf284602976
Jennifer Lane <jennifer.lane610@gmail.com>
2022-04-17 13:59:46 +0100

    added form for two local players inputs, and play again with 2 players button

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,3 +1,4 @@
+from random import choices
 from flask import render_template, request
 from app import app
 from models.player import *
@@ -12,5 +13,21 @@ def play_the_game(player1choice, player2choice):
     player1 = Player("Player 1", player1choice)
     player2 = Player("Player 2", player2choice)
     result = play_game(player1, player2)
-    return render_template('index.html', title="Results", result=result, player1=player1, player2=player2)
+    return render_template('result.html', title="Results are in!", result=result, player1=player1, player2=player2)
+
+@app.route('/2players')
+def two_player_input():
+    return render_template('index.html', title="2 Player Game", choices_list=choices_list)
+
+@app.route('/play', methods=["POST"])
+def two_player_play_game():
+    player1_name = request.form["player1_name"]
+    player1_choice = request.form["player1_choice"]
+    player2_name = request.form["player2_name"]
+    player2_choice = request.form["player2_choice"]
+    player1 = Player(player1_name, player1_choice)
+    player2 = Player(player2_name, player2_choice)
+    result = play_game(player1, player2)
+
+    return render_template('result.html', title="2 Player Game - Results are in!", choices_list=choices_list, player1=player1, player2=player2, result=result)
 
